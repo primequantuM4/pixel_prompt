1c7a3feba340d2ca8a6d238357591e22c63dfd3a
primequantuM4 <gediezra@gmail.com>
2025-06-03 20:03:22 +0300

    Initial commit

---
+++ bin/pixel_prompt.dart
@@ -0,0 +1,5 @@
+import 'package:pixel_prompt/pixel_prompt.dart' as pixel_prompt;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pixel_prompt.calculate()}!');
+}
---
+++ test/pixel_prompt_test.dart
@@ -0,0 +1,8 @@
+import 'package:pixel_prompt/pixel_prompt.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
