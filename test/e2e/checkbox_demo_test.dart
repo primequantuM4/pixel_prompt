2ed267aaa5d39af5179cc719d414015fb286b69e
primequantuM4 <gediezra@gmail.com>
2025-07-05 12:06:20 +0300

    test: rewrite checkbox and textfield e2e tests to use tracing

--- test/e2e/checkbox_demo_test.dart
+++ test/e2e/checkbox_demo_test.dart
@@ -4,14 +4,20 @@ import 'dart:io';
 
 import 'package:test/test.dart';
 
+import '../utils/test_utils.dart';
+
+final _traceRegex =
+    RegExp(r'^==PIXEL_PROMPT_TRACING_(\w+)==\[(.*?)\]\[(.*?)\] (.*)$');
+
 void main() {
   group('Checkbox demo E2E', () {
-    test('Should match golden file before and after toggle', () async {
+    test('Checkbox golden before and after toggle', () async {
       final process = await Process.start(
           'dart',
           [
             'example/interactable_component_demo/bin/checkbox_demo.dart',
           ],
+          environment: {'PIXEL_PROMPT_TRACING': '1'},
           runInShell: true);
 
       final outputLines = <String>[];
@@ -19,62 +25,61 @@ void main() {
 
       int step = 0;
 
-      late final StreamSubscription<String> sub;
+      late final StreamSubscription<String> stdoutSub;
+      late final StreamSubscription<String> stderrSub;
 
-      sub = process.stdout
+      stdoutSub = process.stdout
           .transform(utf8.decoder)
           .transform(const LineSplitter())
           .listen((line) async {
-        outputLines.add(line);
+        if (!_traceRegex.hasMatch(line)) {
+          outputLines.add(line);
+        }
+      });
+
+      stderrSub = process.stderr
+          .transform(utf8.decoder)
+          .transform(const LineSplitter())
+          .listen((line) async {
+        final match = _traceRegex.firstMatch(line);
+
+        if (match == null) return;
+
+        // 1 -> level, ie logger TRACE, WARN, INFO, ERROR
+        // 2 -> ISO timestamp
+        // 3 -> tag, ie App, CanvasBuffer etc..
+
+        final message = match[4];
 
         switch (step) {
           case 0:
-            if (line.contains('--READY--')) {
-              Future.delayed(Duration(milliseconds: 5000), () async {
-                await File(
-                  'test/golden/checkbox_before_toggle.txt',
-                ).writeAsString(outputLines.join('\n'));
+            if (message == 'RENDERED') {
+              final before = outputLines.join('\n');
+              await compareOrUpdateGolden(
+                path: 'test/golden/checkbox_before_toggle.txt',
+                actual: before,
+              );
+              await Future.delayed(Duration(milliseconds: 200));
+              process.stdin.write('\t');
 
-                await Future.delayed(Duration(milliseconds: 200));
-                process.stdin.write('\t');
-
-                final goldenBefore = await File(
-                  'test/golden/checkbox_before_toggle.txt',
-                ).readAsString();
-                final actualBefore = outputLines.join('\n');
-
-                expect(
-                  goldenBefore.trim(),
-                  equals(actualBefore.trim()),
-                  reason: 'Screen before toggle does not match golden',
-                );
-                step++;
-                outputLines.clear();
-              });
+              step++;
+              outputLines.clear();
             }
             break;
           case 1:
-            if (line.contains('[ ]')) {
-              outputLines.clear();
-            } else if (line.contains('--RENDERED--')) {
+            if (message == 'RENDERED') {
               process.stdin.write(' ');
               outputLines.clear();
               step++;
             }
 
           case 2:
-            if (line.contains('[-]')) {
-              await Future.delayed(Duration(milliseconds: 100));
+            if (message == 'RENDERED') {
+              final after = outputLines.join('\n');
 
-              final goldenAfter = await File(
-                'test/golden/checkbox_after_toggle.txt',
-              ).readAsString();
-              final actualAfter = outputLines.join('\n');
-
-              expect(
-                actualAfter.trim(),
-                equals(goldenAfter.trim()),
-                reason: 'Screen after toggle does not match golden',
+              await compareOrUpdateGolden(
+                path: 'test/golden/checkbox_after_toggle.txt',
+                actual: after,
               );
 
               await Future.delayed(Duration(milliseconds: 200));
@@ -94,7 +99,8 @@ void main() {
       });
 
       await completer.future.timeout(Duration(seconds: 50));
-      await sub.cancel();
+      await stdoutSub.cancel();
+      await stderrSub.cancel();
       process.kill();
     });
   });
