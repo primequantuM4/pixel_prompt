1ebea46b2c794eeb96bbfa60f2e5a29e1cdb8a70
primequantuM4 <gediezra@gmail.com>
2025-07-18 18:58:18 +0300

    fix: `TextComponentStyle` to be `const` friendly

--- lib/components/text_component_style.dart
+++ lib/components/text_component_style.dart
@@ -1,110 +1,45 @@
 import 'package:pixel_prompt/logger/logger.dart';
-
 import 'colors.dart';
 import 'font_style.dart';
 
 class TextComponentStyle {
-  AnsiColorType? color;
-  AnsiColorType? bgColor;
-  Set<FontStyle> styles = {};
+  final AnsiColorType? color;
+  final AnsiColorType? bgColor;
+  final Set<FontStyle> styles;
 
-  int _leftPadding = 0;
-  int _rightPadding = 0;
-  int _topPadding = 0;
-  int _bottomPadding = 0;
+  final int _leftPadding;
+  final int _rightPadding;
+  final int _topPadding;
+  final int _bottomPadding;
 
-  int _leftMargin = 0;
-  int _rightMargin = 0;
-  int _topMargin = 0;
-  int _bottomMargin = 0;
+  final int _leftMargin;
+  final int _rightMargin;
+  final int _topMargin;
+  final int _bottomMargin;
 
-  TextComponentStyle foreground(AnsiColorType color) {
-    this.color = color;
-    return this;
-  }
+  const TextComponentStyle({
+    this.color,
+    this.bgColor,
+    Set<FontStyle>? styles,
+    int leftPadding = 0,
+    int rightPadding = 0,
+    int topPadding = 0,
+    int bottomPadding = 0,
+    int leftMargin = 0,
+    int rightMargin = 0,
+    int topMargin = 0,
+    int bottomMargin = 0,
+  })  : styles = styles ?? const <FontStyle>{},
+        _leftPadding = leftPadding,
+        _rightPadding = rightPadding,
+        _topPadding = topPadding,
+        _bottomPadding = bottomPadding,
+        _leftMargin = leftMargin,
+        _rightMargin = rightMargin,
+        _topMargin = topMargin,
+        _bottomMargin = bottomMargin;
 
-  TextComponentStyle background(AnsiColorType color) {
-    bgColor = color;
-    return this;
-  }
-
-  TextComponentStyle bold() {
-    styles.add(FontStyle.bold);
-    return this;
-  }
-
-  TextComponentStyle italic() {
-    styles.add(FontStyle.italic);
-    return this;
-  }
-
-  TextComponentStyle underline() {
-    styles.add(FontStyle.underline);
-    return this;
-  }
-
-  TextComponentStyle strikethrough() {
-    styles.add(FontStyle.strikethrough);
-    return this;
-  }
-
-  TextComponentStyle dim() {
-    Logger.trace("TextComponentStyle", "Dim added ${FontStyle.dim.code}");
-    styles.add(FontStyle.dim);
-    return this;
-  }
-
-  TextComponentStyle paddingTop(int padding) {
-    _topPadding = padding;
-    return this;
-  }
-
-  TextComponentStyle paddingLeft(int padding) {
-    _leftPadding = padding;
-    return this;
-  }
-
-  TextComponentStyle paddingRight(int padding) {
-    _rightPadding = padding;
-    return this;
-  }
-
-  TextComponentStyle paddingBottom(int padding) {
-    _bottomPadding = padding;
-    return this;
-  }
-
-  TextComponentStyle marginTop(int margin) {
-    _topMargin = margin;
-    return this;
-  }
-
-  TextComponentStyle marginBottom(int margin) {
-    _bottomMargin = margin;
-    return this;
-  }
-
-  TextComponentStyle marginLeft(int margin) {
-    _leftMargin = margin;
-    return this;
-  }
-
-  TextComponentStyle marginRight(int margin) {
-    _rightMargin = margin;
-    return this;
-  }
-
-  String getStyleAnsi() {
-    final codes = [
-      for (var style in styles) style.code,
-      if (color != null) color!.fg,
-      if (bgColor != null) bgColor!.bg,
-    ].join(';');
-
-    Logger.trace("TextComponentStyle", codes.split(';').join(':'));
-    return codes;
-  }
-
+  // Getter methods (unchanged)
   int get horizontalPadding => _leftPadding + _rightPadding;
   int get verticalPadding => _topPadding + _bottomPadding;
   int get horizontalMargin => _leftMargin + _rightMargin;
@@ -119,4 +54,80 @@ class TextComponentStyle {
   int get rightMargin => _rightMargin;
   int get topMargin => _topMargin;
   int get bottomMargin => _bottomMargin;
+
+  // Style modification methods (return new instances)
+  TextComponentStyle foreground(AnsiColorType color) => copyWith(color: color);
+
+  TextComponentStyle background(AnsiColorType color) =>
+      copyWith(bgColor: color);
+
+  TextComponentStyle bold() => copyWith(styles: {...styles, FontStyle.bold});
+
+  TextComponentStyle italic() =>
+      copyWith(styles: {...styles, FontStyle.italic});
+
+  TextComponentStyle underline() =>
+      copyWith(styles: {...styles, FontStyle.underline});
+
+  TextComponentStyle strikethrough() =>
+      copyWith(styles: {...styles, FontStyle.strikethrough});
+
+  TextComponentStyle dim() {
+    Logger.trace("TextComponentStyle", "Dim added ${FontStyle.dim.code}");
+    return copyWith(styles: {...styles, FontStyle.dim});
+  }
+
+  // Padding/margin modification methods
+  TextComponentStyle paddingTop(int padding) => copyWith(topPadding: padding);
+  TextComponentStyle paddingLeft(int padding) => copyWith(leftPadding: padding);
+  TextComponentStyle paddingRight(int padding) =>
+      copyWith(rightPadding: padding);
+  TextComponentStyle paddingBottom(int padding) =>
+      copyWith(bottomPadding: padding);
+
+  TextComponentStyle marginTop(int margin) => copyWith(topMargin: margin);
+  TextComponentStyle marginBottom(int margin) => copyWith(bottomMargin: margin);
+  TextComponentStyle marginLeft(int margin) => copyWith(leftMargin: margin);
+  TextComponentStyle marginRight(int margin) => copyWith(rightMargin: margin);
+
+  // ANSI style generator (unchanged)
+  String getStyleAnsi() {
+    final codes = [
+      for (var style in styles) style.code,
+      if (color != null) color!.fg,
+      if (bgColor != null) bgColor!.bg,
+    ].join(';');
+
+    Logger.trace("TextComponentStyle", codes.split(';').join(':'));
+    return codes;
+  }
+
+  // CopyWith helper
+  TextComponentStyle copyWith({
+    AnsiColorType? color,
+    AnsiColorType? bgColor,
+    Set<FontStyle>? styles,
+    int? leftPadding,
+    int? rightPadding,
+    int? topPadding,
+    int? bottomPadding,
+    int? leftMargin,
+    int? rightMargin,
+    int? topMargin,
+    int? bottomMargin,
+  }) {
+    return TextComponentStyle(
+      color: color ?? this.color,
+      bgColor: bgColor ?? this.bgColor,
+      styles: styles ?? this.styles,
+      leftPadding: leftPadding ?? _leftPadding,
+      rightPadding: rightPadding ?? _rightPadding,
+      topPadding: topPadding ?? _topPadding,
+      bottomPadding: bottomPadding ?? _bottomPadding,
+      leftMargin: leftMargin ?? _leftMargin,
+      rightMargin: rightMargin ?? _rightMargin,
+      topMargin: topMargin ?? _topMargin,
+      bottomMargin: bottomMargin ?? _bottomMargin,
+    );
+  }
 }
