4d3586affb243746c01dee880cb08b87ab3e1f1b
primequantuM4 <gediezra@gmail.com>
2025-08-04 17:19:49 +0300

    fix: refactor and adding static const for necessary variables

--- lib/components/font_style.dart
+++ lib/components/font_style.dart
@@ -1,9 +1,32 @@
-enum FontStyle {
-  bold(1),
-  italic(3),
-  underline(4),
-  strikethrough(9);
-
+class FontStyle {
   final int code;
-  const FontStyle(this.code);
+  final String name;
+
+  const FontStyle._(this.code, this.name);
+
+  static const bold = FontStyle._(1, 'bold');
+  static const italic = FontStyle._(3, 'italic');
+  static const underline = FontStyle._(4, 'underline');
+  static const strikethrough = FontStyle._(9, 'strikethrough');
+
+  static const mapping = <int, FontStyle>{
+    1: bold,
+    3: italic,
+    4: underline,
+    9: strikethrough,
+  };
+
+  static const List<FontStyle> values = [
+    bold,
+    italic,
+    underline,
+    strikethrough,
+  ];
+
+  factory FontStyle.fromCode(int code) {
+    return mapping[code]!;
+  }
+
+  @override
+  String toString() => name;
 }
