9b564341612381695fb0b3f11563f3d9fd3d966f
primequantuM4 <gediezra@gmail.com>
2025-08-09 12:57:22 +0300

    docs: add documentation for the directory `lib/common/`

--- lib/common/response_input.dart
+++ lib/common/response_input.dart
@@ -1,10 +1,22 @@
 import 'package:pixel_prompt/core/component_instance.dart';
 
+/// Represents the type of response command returned
+/// after processing an input event.
 enum ResponseCommands { none, exit }
 
+/// Encapsulates the result of processing an input event.
+///
+/// A [ResponseInput] specifies whether an event was handled,
+/// any response commands to execute, and optionally which
+/// [ComponentInstance]s need to be re-rendered.
 class ResponseInput {
+  /// The command to be executed after processing the event.
   final ResponseCommands commands;
+
+  /// Indicates whether the event was handled.
   final bool handled;
+
+  /// The list of [ComponentInstance]s that require a redraw.
   final List<ComponentInstance>? dirty;
 
   const ResponseInput({
@@ -13,6 +25,7 @@ class ResponseInput {
     this.dirty,
   });
 
+  /// Creates a [ResponseInput] indicating the event was ignored.
   static ResponseInput ignored() {
     return ResponseInput(commands: ResponseCommands.none, handled: false);
   }
